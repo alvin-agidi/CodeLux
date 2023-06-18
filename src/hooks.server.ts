8ae9ab3b5909535a252544915b5f691c770ac6fd
alvin-agidi <alvin.agidi@gmail.com>
2023-06-18 12:47:52 +0100

    Changed firebase API to mine

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,13 +7,13 @@ import { initializeApp } from 'firebase/app';
 //firebase initialisation
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyB_3Kbe8DtKqW12tkDjNiLNblkru7JyhKU',
-	authDomain: 'pimp-my-project.firebaseapp.com',
-	projectId: 'pimp-my-project',
-	storageBucket: 'pimp-my-project.appspot.com',
-	messagingSenderId: '1047408725170',
-	appId: '1:1047408725170:web:5140028a78519f379d0dab',
-	measurementId: 'G-GH4JL70L29'
-};
+	apiKey: "AIzaSyB-jeB37I6q5aHkja30qS30TmvPzQ-DtRo",
+	authDomain: "codelux-df0db.firebaseapp.com",
+	projectId: "codelux-df0db",
+	storageBucket: "codelux-df0db.appspot.com",
+	messagingSenderId: "1082257655878",
+	appId: "1:1082257655878:web:c8d68c7609c36e0a2fa247",
+	measurementId: "G-0K3NLE7J5D"
+  };  
 
 export const app = initializeApp(firebaseConfig);
